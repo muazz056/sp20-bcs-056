76fcdc76b10cbd3d2162f77000136cdcd8e1cf61
muazz056 <muaxijaz@gmail.com>
2022-11-23 16:45:32 +0500

    done

---
+++ SP20-BCS-056 WEB MID EXAM/custom.js
@@ -0,0 +1,18 @@
+
+
+// when we click on [mathrubhumi] main heading an alert appears saying 'ITS A HOME PAGE' and on console "CLICKED" appears
+
+
+$(document).ready(function () {
+
+    $("#heading1").click(function () {
+        alert("ITS A HOME PAGE");
+        console.log("clicked")
+
+
+
+    });
+
+
+});
+

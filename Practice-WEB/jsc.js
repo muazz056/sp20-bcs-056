ce0cd5c0eea369d610c444d120fd3bf4652d4e72
muazz056 <muaxijaz@gmail.com>
2023-01-10 20:48:17 +0500

    done

---
+++ Practice-WEB/jsc.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+    $("#sbtn").click(function () {
+        alert("help");
+    });
+
+
+
+    $("#text1").click(function () {
+        alert("Already Home page");
+
+    });
+
+    $("#ate").on(click, function () {
+
+        $('#anim').addClass('ahashakeheartache');
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
